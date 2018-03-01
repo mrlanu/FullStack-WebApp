abdb2cf0b4286003ceb5fbbcd7eade447ba03fc8
Serhiy K <mrlanu@gmail.com>
2018-02-28 18:58:32 -0600

    init commit

---
+++ src/main/java/com/lanu/sambamedia/SambaMediaApplication.java
@@ -0,0 +1,12 @@
+package com.lanu.sambamedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SambaMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SambaMediaApplication.class, args);
+	}
+}
