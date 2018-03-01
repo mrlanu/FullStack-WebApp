6da5b781614fd3167ae90fa6e9753f69c8d29730
Serhiy K <mrlanu@gmail.com>
2018-03-01 14:29:08 -0600

    thymeleaf extra

--- src/main/java/com/lanu/sambamedia/config/SecurityConfig.java
+++ src/main/java/com/lanu/sambamedia/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.lanu.sambamedia.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.context.annotation.Bean;
@@ -10,8 +12,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
