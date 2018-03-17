381ab92c6ef4184a6037f225970793cc97f54571
Serhiy K <mrlanu@gmail.com>
2018-03-16 21:51:40 -0500

    SecurityConfig "dataSource" qualifier

--- src/main/java/com/lanu/sambamedia/config/SecurityConfig.java
+++ src/main/java/com/lanu/sambamedia/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -18,6 +19,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 
+    @Qualifier("dataSource")
     @Autowired
     private DataSource dataSource;
 
